8020d976ec89fe37404d735c9c6aa66e6731c5bc
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-02-13 12:19:15 +0100

    wip

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import * as rtl from '@testing-library/react'
 // don't forget to import the components you want to test!
+import App from './App';
 
 test('sanity test', () => {
   // 1
@@ -14,8 +15,29 @@ test('sanity test', () => {
   const wrapper = rtl.render(<span className='hello'>Hello, World</span>)
   // wrapper.debug()
   // 2 using the wrapper we capture an element by "readable" something
-  const greeting = wrapper.queryByText(/lady gaga/i)
+  const greeting = wrapper.queryByText(/hello, world/i)
 
   // 3 assert the thing with said readable something is actually there
   expect(greeting).toBeInTheDocument()
+})
+
+describe('App', () => {
+  it('renders an element that reads have fun', () => {
+    // 1- setup, render app to the virtual dom
+    const wrapper = rtl.render(<App friend={{ name: 'Zac' }} />)
+    // 2- capture the element of interest
+    const elementWithHaveFun = wrapper.queryByText(/have fun/i)
+    // wrapper.debug()
+    // 3- assert thing in DOM
+    expect(elementWithHaveFun).toBeInTheDocument()
+  })
+
+  it('renders an input with a label "Username"', () => {
+    // 1- setup, render app to the virtual dom
+    const wrapper = rtl.render(<App friend={{ name: 'Zac' }} />)
+    // 2- capture the element of interest
+    const withLabelUsername = wrapper.queryByLabelText(/Username/i) // use the ones with "query"
+    // 3- assert thing in DOM
+    expect(withLabelUsername).toBeInTheDocument()
+  })
 })
