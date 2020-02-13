50b7db38a2a281cf7b560c194cd7bbc02489e009
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-02-13 10:30:54 +0100

    wip

--- src/helpers/index.test.js
+++ src/helpers/index.test.js
@@ -1,6 +1,10 @@
 // don't forget to import the functions you want to test!
 
-test('sanity test', () => {
+// this is a test
+it('is seven for realz', () => {
+  // assertions
   expect(7).toBe(7)
+  expect(7).toBeGreaterThan(6)
   expect(7).not.toBe(8)
+  expect(7).not.toBeUndefined()
 })
