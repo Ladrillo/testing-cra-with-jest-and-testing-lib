0565ba17311f7469ada967e321207a719f31c718
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-02-13 10:37:30 +0100

    wip

--- src/helpers/index.test.js
+++ src/helpers/index.test.js
@@ -1,10 +1,25 @@
 // don't forget to import the functions you want to test!
+import * as help from './index'
 
 // this is a test
-it('is seven for realz', () => {
+it('sanity check', () => {
   // assertions
   expect(7).toBe(7)
   expect(7).toBeGreaterThan(6)
   expect(7).not.toBe(8)
   expect(7).not.toBeUndefined()
+})
+
+describe('helpers module', () => {
+  describe('five function', () => {
+
+  })
+
+  describe('sum', () => {
+
+  })
+
+  describe('personMaker', => {
+    
+  })
 })
