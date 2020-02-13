7e55b36f02aab13e9a23c63d4e0c038ab9976125
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-02-13 10:44:14 +0100

    wip

--- src/helpers/index.test.js
+++ src/helpers/index.test.js
@@ -12,7 +12,21 @@ it('sanity check', () => {
 
 describe('helpers module', () => {
   describe('five function', () => {
+    it('returns five', () => {
+      expect(help.five()).toBe(5) // toEqual with [] {}
+    })
+    it('returns a value that is defined', () => {
 
+    })
+    it('returs a value greater than 4', () => {
+
+    })
+    it('returs a value not NaN', () => {
+      
+    })
+    it('returs a value smaller than 6', () => {
+      
+    })
   })
 
   describe('sum', () => {
